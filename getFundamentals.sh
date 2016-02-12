5398c6947cce169f545bebefd059b2623a323d9d
Alessio Gottardo <alessio.gottardo@axonix.com>
2016-02-12 14:09:35 +0000

    more data on fundamentals

--- getFundamentals.sh
+++ getFundamentals.sh
@@ -9,8 +9,12 @@ tickers_pay=( 'PYPL' '2PP' 'SQ' )
 tickers_it=( 'GOOG' 'GOOGL' 'AAPL' 'TEAM' 'GDDY' )
 tickers_health=( 'FIT' )
 
+FUNDAMENTALS_DIR='data/fundamentals/'
+FUNDAMENTALS_LOG_DIR=${FUNDAMENTALS_DIR}logs/
+mkdir -p ${FUNDAMENTALS_LOG_DIR}
+
 for ticker in "${tickers_clothes[@]}" "${tickers_air[@]}" "${tickers_social[@]}" "${tickers_factory[@]}" "${tickers_online_retail[@]}" "${tickers_pay[@]}" "${tickers_it[@]}" "${tickers_health[@]}" 
 do
   echo "`date +%Y-%m-%d:%H:%M:%S` processing ${ticker}"
-  pystock-crawler reports ${ticker} -o data/fundamentals_${ticker}.csv -s 20060101 -e 20161231 --sort -l data/tickers_${ticker}.log
+  pystock-crawler reports ${ticker} -o ${FUNDAMENTALS_DIR}fundamentals_${ticker}.csv -s 20060101 -e 20161231 --sort -l `${FUNDAMENTALS_LOG_DIR}`tickers_${ticker}.log
 done
