58baddbe03cd6011c62e8c9cf4406e7f1f96414e
Alessio Gottardo <alessio.gottardo@axonix.com>
2016-02-12 14:47:39 +0000

    Refactoring the fetch script

--- getFundamentals.sh
+++ getFundamentals.sh
@@ -15,6 +15,6 @@ mkdir -p ${FUNDAMENTALS_LOG_DIR}
 
 for ticker in "${tickers_clothes[@]}" "${tickers_air[@]}" "${tickers_social[@]}" "${tickers_factory[@]}" "${tickers_online_retail[@]}" "${tickers_pay[@]}" "${tickers_it[@]}" "${tickers_health[@]}" 
 do
-  echo "`date +%Y-%m-%d:%H:%M:%S` processing ${ticker}"
-  pystock-crawler reports ${ticker} -o ${FUNDAMENTALS_DIR}fundamentals_${ticker}.csv -s 20060101 -e 20161231 --sort -l `${FUNDAMENTALS_LOG_DIR}`tickers_${ticker}.log
+  echo "`date +%Y-%m-%d:%H:%M:%S` processing ticker: ${ticker}"
+  pystock-crawler reports ${ticker} -o ${FUNDAMENTALS_DIR}fundamentals_${ticker}.csv -s 20060101 -e 20161231 --sort -l "${FUNDAMENTALS_LOG_DIR}tickers_${ticker}.log"
 done
