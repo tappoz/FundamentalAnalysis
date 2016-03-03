4bddfc4788314a4aa352f66472efa9f9c7524ad6
tappoz <tappoz84@yahoo.it>
2016-03-03 22:03:59 +0000

    WIP extracting paths in a shared module

---
+++ modules/config_details.py
@@ -0,0 +1,13 @@
+
+fundamentals_dir = './data/fundamentals/'
+fundamentals_filename_glob = 'fundamentals_*.csv'
+fundamentals_filename_regex = 'fundamentals_(.*).csv'
+
+stock_prices_dir = './data/stock_prices/'
+stock_prices_ticker_pre_filename_prefix = 'close_prices_pre_2016_02_'
+stock_prices_ticker_post_filename_prefix = 'close_prices_post_2016_02_'
+
+def get_stock_prices_pre_2016_02_filename(ticker):
+  return stock_prices_ticker_pre_filename_prefix + ticker.upper() + '.csv'
+def get_stock_prices_post_2016_02_filename(ticker):
+  return stock_prices_ticker_post_filename_prefix + ticker.upper() + '.csv'
